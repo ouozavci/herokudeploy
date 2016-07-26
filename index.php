97fc197f9c1a471d21ab4961dfd1d5de9e7d3bc8
ouozavci <ouozavci@gmail.com>
2016-07-26 09:32:34 +0300

    js redirect

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-header("Location: https://play.google.com/store/apps/details?id=com.incirrd.bildiricin");
-?>
+<html>
+<head>
+<script>
+    <?php 
+       
+            echo 'top.location="https://play.google.com/store/apps/details?id=com.incirrd.bildiricin";';
+        
+    ?>
+</script>
+<head><body></body>
+</html>

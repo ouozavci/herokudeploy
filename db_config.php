7962b579c71e1d7d1929f345c25095a5e502a94a
ouozavci <ouozavci@gmail.com>
2016-07-27 13:48:20 +0300

    db config

--- db_config.php
+++ db_config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
  
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', ""); // db password (mention your db password here)
-define('DB_DATABASE', "userinformation"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_USER', "sql7128978"); // db user
+define('DB_PASSWORD', "jQh7dVXqwB"); // db password (mention your db password here)
+define('DB_DATABASE', "sql7128978"); // database name
+define('DB_SERVER', "sql7.freemysqlhosting.net"); // db server
 ?>
